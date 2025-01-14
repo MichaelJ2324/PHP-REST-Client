127a76fcafb5701664b216c60c762a1a7411517c
Mike Russell <3056352+MichaelJ2324@users.noreply.github.com>
2025-01-14 12:53:11 -0500

    Abstract Default Endpoints setup + Allow for Versioned Endpoint Name mapping

--- src/Endpoint/Provider/DefaultEndpointProvider.php
+++ src/Endpoint/Provider/DefaultEndpointProvider.php
@@ -4,15 +4,10 @@ namespace MRussell\REST\Endpoint\Provider;
 
 class DefaultEndpointProvider extends AbstractEndpointProvider
 {
-    /**
-     * List of default endpoints to load
-     */
-    protected static array $_DEFAULT_ENDPOINTS = [];
+    use DefaultEndpointsTrait;
 
     public function __construct()
     {
-        foreach (static::$_DEFAULT_ENDPOINTS as $name => $epData) {
-            $this->registerEndpoint($name, $epData[self::ENDPOINT_CLASS], $epData[self::ENDPOINT_PROPERTIES] ?? []);
-        }
+        $this->registerDefaultEndpoints();
     }
 }
