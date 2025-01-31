4a88b9afcda176a9ba05fd6fa05b7c71c35622bb
Mike Russell <3056352+MichaelJ2324@users.noreply.github.com>
2025-01-31 16:40:53 -0500

    Abstract Generating Endpoint inside Endpoint

---
+++ src/Endpoint/Traits/GenerateEndpointTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MRussell\REST\Endpoint\Traits;
+
+use MRussell\REST\Endpoint\Interfaces\EndpointInterface;
+
+trait GenerateEndpointTrait
+{
+    protected function generateEndpoint(string $endpoint): EndpointInterface|null
+    {
+        $EP = null;
+        if (class_exists($endpoint)) {
+            $EP = new $endpoint();
+        } elseif (!empty($this->_client)) {
+            if ($this->_client->hasEndpoint($endpoint)) {
+                $EP = $this->_client->getEndpoint($endpoint);
+            }
+        }
+
+        if ($EP instanceof EndpointInterface) {
+            if (!empty($this->_client)) {
+                $EP->setClient($this->getClient());
+            } else {
+                $EP->setBaseUrl($this->getBaseUrl());
+            }
+        }
+        return $EP;
+    }
+}
